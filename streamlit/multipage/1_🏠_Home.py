4231503cd980e069f2b05e33f3cd6cb704717ccd
young <ykn9080@empal.com>
2024-02-13 13:50:39 +0900

    multipage error

--- streamlit/multipage/1_🏠_Home.py
+++ streamlit/multipage/1_🏠_Home.py
@@ -1,29 +1,26 @@
 import streamlit as st
-import sys
 
-hidemenu = """
-<style>
-    [data-testid="stSidebarNav"]{
-        display:none
-    }
-</style>
-"""
+# hidemenu = """
+# <style>
+#     [data-testid="stSidebarNav"]{
+#         display:none
+#     }
+# </style>
+# """
 
-hidesidebar = """
-<style>
-    [data-testid="stSidebar"]{
-        display:none
-    }
-</style>
-"""
+# hidesidebar = """
+# <style>
+#     [data-testid="stSidebar"]{
+#         display:none
+#     }
+# </style>
+# """
 
 
 def main():
     st.title("💬 Streamlit Tutorial")
     # st.markdown(hidemenu, unsafe_allow_html=True)
-    if st.button("Home"):
-        st.switch_page("your_app.py")
-    if st.button("Page 1"):
+    if st.button("Components"):
         st.switch_page("pages/2_🛠️_Components.py")
 
 
